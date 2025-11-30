7e1c069cd2b4894a4365fdf6e098a69cae3aadc2
Kirill Zaytsev <zaytsev.cmath10@gmail.com>
2025-11-30 17:29:28 +0400

    test: Raised node's minimum version to v22

--- src/runners/Each.ts
+++ src/runners/Each.ts
@@ -9,16 +9,14 @@ import type {
 
 import { isArray } from '@/predicates'
 
-export default (constraints: MaybeMany<Constraint>) => {
-  return {
-    run <F extends Validate | ValidateSync> (
-      validate: F,
-      value: unknown,
-      path: PropertyKey[]
-    ): Validation<F>[] {
-      return isArray(value)
-        ? value.map((v, i) => validate(v, constraints, [...path, i])) as Validation<F>[]
-        : [validate(value, constraints, [...path])] as Validation<F>[]
-    },
-  } as ValidationRunner
-}
+export default (constraints: MaybeMany<Constraint>): ValidationRunner => ({
+  run <F extends Validate | ValidateSync> (
+    validate: F,
+    value: unknown,
+    path: PropertyKey[]
+  ): Validation<F>[] {
+    return isArray(value)
+      ? value.map((v, i) => validate(v, constraints, [...path, i])) as Validation<F>[]
+      : [validate(value, constraints, [...path])] as Validation<F>[]
+  },
+})
